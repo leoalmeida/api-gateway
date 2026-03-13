26f0413ec0498a94716d06ed751dbcc5705f412f
leoalmeida <leo_asf@hotmail.com>
2026-03-13 17:42:37 -0300

    Separação do api-gateway dos outros projetos

---
+++ src/main/java/space/lasf/gateway/GatewayApplication.java
@@ -0,0 +1,12 @@
+package space.lasf.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
